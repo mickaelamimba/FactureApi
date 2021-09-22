2a0cd95d9751364bf1d53feb1787d2e2f726e682
mickaelamimba <81103599+mickaelamimba@users.noreply.github.com>
2021-09-22 17:11:31 -0300

    Update invoice.js
    Update crud route for invoice

--- routes/invoice.js
+++ routes/invoice.js
@@ -1,6 +1,13 @@
-const {invoiceGet} = require("../controllers/invoice");
+const {invoiceGet, invoiceCreate, invoiceUpdate, invoiceDelete} = require("../controllers/invoice");
 const invoiceRoute =(app)=>{
+    //Reade
     app.get('/invoice', invoiceGet)
+    //Update
+    app.post("/invoiceUpdate",invoiceUpdate)
+    //Create
+    app.post("/invoiceCreate", invoiceCreate)
+    //Delete
+    app.delete("/invoiceDelete",invoiceDelete)
 
 }
 
