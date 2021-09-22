37b9fa6f59ddd6a63d97aa8e62bafe13dba69ea0
mickaelamimba <81103599+mickaelamimba@users.noreply.github.com>
2021-09-22 14:01:05 -0300

    Update invoice.js
    update and add new crud  function

--- controllers/invoice.js
+++ controllers/invoice.js
@@ -1,5 +1,56 @@
 const invoiceGet = async (req, res)=>{
-    res.json('tes')
+    try {
+        const Invoice = req.app.get("models").Invoice
+        const MyInvoice = await Invoice.find()
+        res.json(MyInvoice)
+    } catch (error) {
+        res.json(error.message)
+    }
+}
+const invoiceCreate = async (req, res)=>{
+    try {
+        const Invoice = req.app.get("models").Invoice
+        const NewInvoice = await new Invoice({
+            invoiceYear: req.body.invoiceYear,
+            conditions :req.body.conditions,
+            invoiceNumber: req.body.invoiceNumber,
+        }).save()
+        res.json(NewInvoice)
+    } catch (error) {
+        res.json(error.message)
+    }
+}
+const invoiceUpdate = async (req, res)=>{
+    try {
+        if (!req.body._id || !req.body.toModify) {
+            res.json("_id not fund")   
+        }
+        const Invoice = req.app.get("models").Invoice
+        const ToModifyInvoice = await new Invoice.findById(req.body._id)
+        const toModifyKeys = Object.keys(req.body.toModify)
+        for (const key of toModifyKeys) {
+            ToModifyInvoice[key] = req.body.toModify[key]  
+        }
+        await ToModifyInvoice.save()
+    
+        res.json( ToModifyInvoice) 
+    } catch (error) {
+        res.json(error.message)
+    }
+}
+const invoiceDelete = async (req, res)=>{
+    try {
+        if (!req.body._id) {
+            res.json("_id not fund")   
+        }
+        const Invoice = req.app.get("models").Invoice
+        const ToDeleteInvoice = await new Invoice.findById(req.body._id)
+        await ToDeleteInvoice.remove()
+        res.json("susccessfully deleted") 
+    } catch (error) {
+        res.json(error.message)
+    }
 }
 
-module.exports = {invoiceGet}
+
+module.exports = {invoiceGet, invoiceDelete,invoiceUpdate ,invoiceCreate }
