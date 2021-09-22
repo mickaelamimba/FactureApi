177539bc82ba687a920a4d30d7b21b3d11d101be
mickaelamimba <81103599+mickaelamimba@users.noreply.github.com>
2021-09-22 18:09:47 -0300

    Update invoice.js
    add ref in Invoice

--- models/invoice.js
+++ models/invoice.js
@@ -4,6 +4,11 @@ const InvoiceSchema = new mongoose.Schema({
     invoiceYear: {type: Date, required: true},
     conditions :{type: String, required:true},
     invoiceNumber: {type: Number, required: true},
+    article : {type: mongoose.Schema.Types.ObjectId, ref: "Article"},
+    company : {type: mongoose.Schema.Types.ObjectId, ref: "Company"},
+    client: {type: mongoose.Schema.Types.ObjectId, ref: "Client"},
+    payment :{type: mongoose.Schema.Types.ObjectId, ref: "Payment"}
+
 
 })
 
